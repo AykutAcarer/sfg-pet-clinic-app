1780d71558fadfcb5ee750e3514b4430775bb31d
Aykut Acarer <acareraykut@gmail.com>
2023-04-19 17:55:44 +0200

    listed all vets and update controller

--- pet-clinic-web/src/main/java/guru/springframework/petclinicapp/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/petclinicapp/controllers/VetController.java
@@ -1,7 +1,10 @@
 package guru.springframework.petclinicapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.petclinicapp.services.VetService;
 
 /**
  *@author Aykut Acarer
@@ -11,9 +14,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
+
 	@RequestMapping({"/vets", "/vets/vetlist", "/vets/vetlist.html"})
-	public String listVets() {
+	public String listVets(Model model) {
 		
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/vetlist";
 	}
 }
